5a8fc1bea08d0458dc8e338083e40f920f20b391
Seong Joon Oh <coallaoh@google.com>
2019-01-21 14:16:25 +0900

    Update example_visualization.py
    Let arguments be passed from command line.

--- example_visualization.py
+++ example_visualization.py
@@ -25,33 +25,50 @@ from PIL import Image
 
 
 def main():
-  output_dir = 'data'
-  num_digits = 2
-  domain_gap = 'number'
-  mnist_split = 'train'
-  num_visualize = 10
+  parser = argparse.ArgumentParser(
+    description='Visualize created n-digit MNIST dataset.')
+
+  parser.add_argument('--num_digits', default=1, type=int,
+                      help='Number of concatenated digits per data point.')
+
+  parser.add_argument('--domain_gap', default='instance', type=str,
+                      choices=['instance', 'number'],
+                      help='How to split training and test sets'
+                           'of the n-digit mnist.')
+
+  parser.add_argument('--num_visualize', default=10, type=int,
+                      help='How many samples to visualize.')
+
+  parser.add_argument('--output_dir', default='data', type=str,
+                      help='Directory to write the dataset.')
+
+  parser.add_argument('--mnist_split', default='data', type=str,
+                      help='Which MNIST split was used to generate data?'
+                           '(train or test)')
+
+  args = parser.parse_args()
 
   # Construct dataset directory.
-  dataset_dir = os.path.join(output_dir,
-                             'dataset_mnist_%d_%s' % (num_digits,
-                                                      domain_gap))
+  dataset_dir = os.path.join(args.output_dir,
+                             'dataset_mnist_%d_%s' % (args.num_digits,
+                                                      args.domain_gap))
 
-  path = os.path.join(dataset_dir, '%s.npz' % mnist_split)
+  path = os.path.join(args.dataset_dir, '%s.npz' % args.mnist_split)
   with open(path, 'r') as f:
     data = np.load(path)
     labels = data['labels']
     images = data['images']
 
-  visualize_dir = os.path.join(dataset_dir, 'visualization')
+  visualize_dir = os.path.join(args.dataset_dir, 'visualization')
   if not os.path.exists(visualize_dir):
     os.makedirs(visualize_dir)
 
   visualize_indices = np.random.choice(range(len(labels)),
-                                       num_visualize, replace=False)
+                                       args.num_visualize, replace=False)
   
   for i in visualize_indices:
     im = Image.fromarray(images[i])
-    im.save(os.path.join(visualize_dir,
+    im.save(os.path.join(args.visualize_dir,
                          'sample_image_%d_label_%d.jpg' % (i, labels[i])))
 
 if __name__ == '__main__':
